3c952b44396093f93dcbbfd5ea6fef7789124d4d
Dale Hamel <dale.hamel@shopify.com>
2016-05-04 17:31:22 -0400

    Fix

--- lib/ruby-pandoc/converter.rb
+++ lib/ruby-pandoc/converter.rb
@@ -51,8 +51,7 @@ module RubyPandoc
       'odt'   => 'OpenDocument',
       'docx'  => 'Word docx',
       'epub'  => 'EPUB V2',
-      'epub3' => 'EPUB V3',
-      'pdf'   => 'Portable Document File'
+      'epub3' => 'EPUB V3'
     }.freeze
 
     # All of the available Writers.
